269eb7628c1ae0ca97af249841bc3ef02707d435
doori30 <rublju0070@naver.com>
2019-08-20 14:57:42 +0900

    jQuery

---
+++ public/js/index jQuery.js
@@ -0,0 +1,48 @@
+// 전역변수
+var key = '02efdd64bdc14b279bc91d9247db4722';
+var units = 'metric';
+var dailyAPI = 'https://api.openweathermap.org/data/2.5/weather';
+var weeklyAPI = 'https://api.openweathermap.org/data/2.5/forecast';
+//https://api.openweathermap.org/data/2.5/weather(파일명)?(변수를 줌 - get방식)
+//https://api.openweathermap.org/data/2.5/weather?id=1835848&appid=02efdd64bdc14b279bc91d9247db4722&units=metric
+var cityURL ="json/city.json";
+var dailyURL = dailyAPI + '?appid=' + key + '&units=' + units ;
+var weeklyURL = weeklyAPI + '?appid=' + key + '&units=' + units ;
+
+//프로그램시작
+init();
+function init(){
+	$.ajax({
+		type: "get",
+		url: cityURL,
+		dataType: "json",
+		success: cityFn 
+		//	success: function(res) 
+	});
+}
+
+//도시정보 가져오기
+function cityFn(res){
+	var cities = res.cities;
+	$("#cities").empty();
+	$("#cities").append('<option value="" selected>도시를 선택해 주세요!</option>');
+	for(var i in cities){
+			$("#cities").append('<option value="'+cities[i].id+'">'+cities[i].name+'</option>');
+		}
+		$("#cities").change(function(){
+			$.ajax({
+				type: "get",
+				url: dailyURL + "&id=" + $(this).val(),
+				data: "data",
+				dataType: "json",
+				success: dailyFn 
+			});
+		})
+}
+//데일리정보 가져오기
+function dailyFn(res){
+console.log(res);
+}
+
+
+
